1fd70341e224920dc2bdd1007b8b811d485a3c18
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-03-27 10:25:59 +0100

    * fix

--- src/misaxx-analyzer/module_info.cpp
+++ src/misaxx-analyzer/module_info.cpp
@@ -3,14 +3,14 @@
 #include <misaxx-analyzer/module_info.h>
 
 misaxx::misa_module_info misaxx_analyzer::module_info() {
-    misaxx::misa_mutable_module_info info;
+    misaxx::misa_module_info info;
     info.set_id("misaxx-analyzer");
     info.set_version("1.0.0");
     info.set_name("MISA++ Result Analyzer");
     info.set_description("Companion module for MISA-ImageJ that applies performance-critical tasks");
 
     // External dependency: OME Files
-    misaxx::misa_mutable_module_info sqlite_info;
+    misaxx::misa_module_info sqlite_info;
     sqlite_info.set_id("sqlite3");
     sqlite_info.set_name("SQLite");
     sqlite_info.set_version("3");
